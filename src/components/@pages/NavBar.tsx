afcbef0ef420c3cbb7b8eac58fc6073623afc935
michelbeyrouty <michelbeyrouty@gmail.com>
2025-12-08 21:28:25 +0100

    feat: update nav

--- src/components/@pages/NavBar.tsx
+++ src/components/@pages/NavBar.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Link from "next/link";
+import { FaGithub, FaLinkedin } from "react-icons/fa6";
 import LogoButton from "@common/Buttons/LogoButton";
 
 export default function Navbar() {
@@ -17,10 +18,26 @@ export default function Navbar() {
       link: "/#contact",
       title: "Contact",
     },
+    {
+      link: "https://github.com/michelbeyrouty",
+      title: <FaGithub />,
+    },
+    {
+      link: "https://linkedin.com/in/michelbeyrouty",
+      title: <FaLinkedin />,
+    },
+    {
+      link: "https://drive.google.com/file/d/1KUEhL7KTxjHwM0h_fMtunx-1Jdo-DEp0/view?usp=sharing",
+      title: "Resume",
+    },
   ];
 
   return (
     <nav className="flex flex-row justify-between px-6 py-5 w-full items-center fixed top-0 z-20 bg-brand-black">
+      <Link href="/">
+        <LogoButton text="Michel_Beyrouty" />
+      </Link>
+
       <ul className="flex space-x-8 items-center">
         {navLinks.map((nav) => (
           <li key={nav.link}>
@@ -33,10 +50,6 @@ export default function Navbar() {
           </li>
         ))}
       </ul>
-
-      <Link href="/">
-        <LogoButton text="Michel_Beyrouty" />
-      </Link>
     </nav>
   );
 }
